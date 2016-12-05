7c077e5c5a14627c19df806c2a42c9f0c185a942
jday16 <janessaday@eagles.ewu.edu>
2016-12-05 11:53:20 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 $('body').addClass( 'bg' + 'header'+ randNum );
 
 // Store & Generate Number
-var maxRand = 3;
+var maxRand = 5;
 var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
@@ -13,7 +13,7 @@ var img0 = '/img/fullsize/tink.jpg';
 var img1 = '/img/fullsize/boots.jpg';
 var img2 = '/img/fullsize/earth_girl.jpg';
 var img3 = '/img/fullsize/flower.jpg';
-var img3 = '/img/fullsize/elephant.jpg';
+var img4 = '/img/fullsize/elephant.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );
